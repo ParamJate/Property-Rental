446809b9845a6a2887888e3ab09bfa8de21a4a7b
ParamJate <jateparam3@gmail.com>
2024-02-28 16:54:48 +0530

    client commit

---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,115 @@
+import React, { useEffect, useState } from 'react'
+import {useNavigate} from "react-router-dom"
+import '../styles/Register.scss'
+
+const RegisterPage = () => {
+  
+  const[formData, setFormData] = useState({
+    firstName: "",
+    lastName:"",
+    email:"",
+    password:"",
+    confirmPassword:""
+  })
+
+  const handleChange = (e) =>{
+    const {name, value} = e.target
+    setFormData({
+      ...formData,
+      [name]:value
+    })
+  }
+  const [passMatch, setPassMatch] = useState(true)
+  
+  useEffect(()=>{
+    setPassMatch(formData.password === formData.confirmPassword || formData.confirmPassword==="")
+  })
+
+  const navigate = useNavigate()
+
+  const handleSubmit = (e)=>{
+    e.preventDefault()
+
+    try {
+      const register_form = new FormData()
+
+      for(var key in register_form){
+        register_form.append(key, formData[key])
+      }
+
+      const response = fetch('https://localhost:3001/auth/register', {
+        method: "POST",
+        body: register_form
+      })
+
+      if (response.ok){
+        navigate('/login')
+      }
+    } catch(err){
+      console.log("Registration failed", err.message)
+    }
+  }
+
+  
+  return (
+    <div className='register'>
+        <div className="register_content">
+            <form className="register_content_form" onSubmit={handleSubmit}>
+                <input 
+                className='floating-label'
+                placeholder='First Name'
+                type="text" 
+                name='firstName'
+                value={formData.firstName}
+                onChange={handleChange}
+                required
+                />
+                <input 
+                className='floating-label'
+                type="text" 
+                name='lastName'
+                value={formData.lastName}
+                placeholder='Last Name'
+                onChange={handleChange}
+                required
+                />
+                <input 
+                className='floating-label'
+                type="email" 
+                placeholder='Email'
+                name='email'
+                value={formData.email}
+                onChange={handleChange}
+                required
+                />
+                <input 
+                className='floating-label'
+                type="passord" 
+                placeholder='Password'
+                name='password'
+                value={formData.password}
+                onChange={handleChange}
+                required
+                />
+                <input 
+                className='floating-label'
+                type="password" 
+                placeholder='Confirm Password'
+                name='confirmPassword'
+                value={formData.confirmPassword}
+                onChange={handleChange}
+                required
+                />
+                {!passMatch && (
+                  <p style={{color: 'red'}}>Password doesn't match!</p>
+                )}
+
+              <button type='submit' disabled={!passMatch}>REGISTER</button>
+            </form>
+            <a href="/login" className='hover-underline-animation'>Already have an Account? Log In Here</a>
+        </div>
+    </div>
+  )
+}
+
+export default RegisterPage
---
+++ server/routes/auth.js
@@ -0,0 +1,37 @@
+const router = require('express').Router()
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const multer = require('multer')
+
+const User = require('../models/User')
+
+router.post('/register', async(req, res) =>{
+    try{
+        const { firstName, lastName, email, password } = req.body
+
+        const existingUser = await User.findOne({ email })
+        if (existingUser){
+            return res.status(409).json({ message: "User already exists!"})
+        }
+
+        const salt = await bcrypt.genSalt()
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        const newUser = new User ({
+            firstName,
+            lastName,
+            email,
+            password: hashedPassword
+        })
+
+        await newUser.save()
+
+        //sending success msg
+        
+        res.status(200).json({ message:"User registered successfully!", user: newUser})
+    } catch(err){}
+        console.log(err)
+        res.status(500).json({ message: "registartion failed!", error: err.messasge})  
+})
+
+module.exports = router
